b380d8f5ad249fb67c01c4ca0facf3bf1f3bf14e
CHARITHA  M <charitha.m@s.amity.edu>
2025-04-03 19:16:46 +0530

    first commit

---
+++ operator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	double num1,num2;
+	char operator;
+
+	printf("enter first number:");
+	scanf("%|f",&num1);
+	printf("enter second number:");
+	scanf("%|f",&num2);
+
+	printf("addition:%.2|f\n",num1+num2);
+
+        return 0;
+
+}
