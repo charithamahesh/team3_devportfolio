3cdc80283b212557096c753817dc4a5e3dca2a73
CHARITHA  M <charitha.m@s.amity.edu>
2025-04-03 19:52:49 +0530

    added new line is test branch to divide two numbers

--- operator.c
+++ operator.c
@@ -8,11 +8,10 @@ int main() {
 	scanf("%|f",&num1);
 	printf("enter second number:");
 	scanf("%|f",&num2);
-
-	printf("addition:%.2|f\n",num1+num2);
+        printf("addition:%.2|f\n",num1+num2);
 	printf("subtraction:%.2|f\n",num1-num2);
 	printf("multiplication:%.2|f\n",num1*num2);
-
-        return 0;
+	printf("division:%.2|f\n",num1/num2);
+	return 0;
 
 }
